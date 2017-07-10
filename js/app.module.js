2ed55c93931d8d70aceadddc14f3538701ea27db
Shayna <shaynawark@gmail.com>
2017-07-10 13:32:23 -0400

    added functionality
    none checked, no actual webpage, just the app.module.js content

---
+++ js/app.module.js
@@ -0,0 +1,27 @@
+angular
+    .module('SmallFirebaseApp', ['firebase'])
+    .constant('firebaseConfig', {
+        apiKey: "AIzaSyA2snvldJm9sPJT-0FCSnxnPeaB3jQEMTc",
+        authDomain: "webapps-9058d.firebaseapp.com",
+        databaseURL: "https://webapps-9058d.firebaseio.com",
+        projectId: "webapps-9058d",
+        storageBucket: "webapps-9058d.appspot.com",
+        messagingSenderId: "668287105690"
+    })
+    .run(firebaseConfig => firebase.initializeApp(firebaseConfig))
+    .controller('ContactCtrl', function($scope, $firebaseObject, $firebaseArray){
+        const dbRef = firebase.database().ref().child('contacts')
+        $scope.contactList = $firebaseArray(dbRef)
+        this.getBlankContact = () => ({
+            title: '',
+            dueDate: '',
+            isComplete: false
+        })
+        $scope.newContact = this.getBlankContact()
+        $scope.addContact = () => {
+            $scope.contactList.$add($scope.newContact);
+            $scope.newContact = this.getBlankContact()
+        }
+        $scope.saveContact = contact => $scope.contactList.$save(contact)
+        $scope.removeContact = contact => $scope.contactList.$remove(contact)
+    })
